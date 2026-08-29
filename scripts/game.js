b102b20d4ea455ad4ca493fabad582fe879c5410
hszrdy <63145494+hszrdy@users.noreply.github.com>
2026-08-30 02:01:26 +0800

    fixed typo and added basic IIFE & functions
    fixed typo of yWinds to yWins, added function for generating UI cells for grid, wrapped all into a IIFE function

--- scripts/game.js
+++ scripts/game.js
@@ -1,112 +1,134 @@
-const xPlayerWinCondition = 3;
-const yPlayerWinCondition = -3;
+(function () {
+    const xPlayerWinCondition = 3;
+    const yPlayerWinCondition = -3;
+    let turn = 1; //1 for x, -1 for y
 
-function Gameboard() {
-    if (!new.target) {
-        return new Gameboard();
-    }
-
-    this.board = [
-        [1, -1, -1],
-        [-1, -1, 1],
-        [-1, 1, 1]
-    ];
-}
-
-function CheckBoardCondition(gameboard) {
-
-
-    //check rows
-    for (let i = 0; i < gameboard.board.length; i++) {
-        let rowsum = 0;
-        for (let j = 0; j < gameboard.board[i].length; j++) {
-            if (gameboard.board[i][j] === 1) {
-                rowsum++;
-            }
-            else if (gameboard.board[i][j] === -1) {
-                rowsum--;
+    function GenerateUIBoard() {
+        const gridContainer = document.querySelector('.grid-container');
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const cell = document.createElement('div');
+                cell.addEventListener('click', (e) => {
+                    cell.textContent = turn === 1 ? 'X' : 'O';
+                    turn *= -1;
+                });
+                cell.classList.add('grid-item');
+                cell.dataset.row = i;
+                cell.dataset.col = j;
+                gridContainer.appendChild(cell);
             }
         }
-
-        if (rowsum == 3) {
-            return { xWins: true, yWins: false }
-        }
-        else if (rowsum == -3) {
-            return { xWins: false, yWins: true }
-        }
     }
 
-    //check columns
-    for (let i = 0; i < gameboard.board.length; i++) {
-        let colsum = 0;
-        for (let j = 0; j < gameboard.board[i].length; j++) {
-            if (gameboard.board[j][i] === 1) {
-                colsum++;
+    function Gameboard() {
+        if (!new.target) {
+            return new Gameboard();
+        }
+
+        GenerateUIBoard();
+        this.board = [
+            [1, -1, -1],
+            [-1, -1, 1],
+            [-1, 1, 1]
+        ];
+    }
+
+    function CheckBoardCondition(gameboard) {
+
+
+        //check rows
+        for (let i = 0; i < gameboard.board.length; i++) {
+            let rowsum = 0;
+            for (let j = 0; j < gameboard.board[i].length; j++) {
+                if (gameboard.board[i][j] === 1) {
+                    rowsum++;
+                }
+                else if (gameboard.board[i][j] === -1) {
+                    rowsum--;
+                }
             }
-            else if (gameboard.board[j][i] === -1) {
-                colsum--;
+
+            if (rowsum === 3) {
+                return { xWins: true, yWins: false }
+            }
+            else if (rowsum === -3) {
+                return { xWins: false, yWins: true }
             }
         }
 
-        if (colsum == 3) {
-            return { xWins: true, yWins: false }
+        //check columns
+        for (let i = 0; i < gameboard.board.length; i++) {
+            let colsum = 0;
+            for (let j = 0; j < gameboard.board[i].length; j++) {
+                if (gameboard.board[j][i] === 1) {
+                    colsum++;
+                }
+                else if (gameboard.board[j][i] === -1) {
+                    colsum--;
+                }
+            }
+
+            if (colsum === 3) {
+                return { xWins: true, yWins: false }
+            }
+            else if (colsum === -3) {
+                return { xWins: false, yWins: true }
+            }
         }
-        else if (colsum == -3) {
-            return { xWins: false, yWins: true }
+
+        //check left root diagonals
+        let leftdiagsum = 0;
+        let rightdiagsum = 0;
+        for (let i = 0; i < gameboard.board.length; i++) {
+            if (gameboard.board[i][i] === 1) {
+                leftdiagsum++;
+            }
+            else if (gameboard.board[i][i] === -1) {
+                leftdiagsum--;
+            }
+
+            if (leftdiagsum === 3) {
+                return { xWins: true, yWins: false }
+            }
+            else if (leftdiagsum === -3) {
+                return { xWins: false, yWins: true }
+            }
+        }
+
+        for (let i = 0; i < gameboard.board.length; i++) {
+            let n = gameboard.board[i].length
+            if (gameboard.board[i][n - 1 - i] === 1) {
+                rightdiagsum++;
+            }
+            else if (gameboard.board[i][n - 1 - i] === -1) {
+                rightdiagsum--;
+            }
+
+            console.log(rightdiagsum)
+            if (rightdiagsum === 3) {
+                return { xWins: true, yWins: false }
+            }
+            else if (rightdiagsum === -3) {
+                return { xWins: false, yWins: true }
+            }
         }
     }
 
-    //check left root diagonals
-    let leftdiagsum = 0;
-    let rightdiagsum = 0;
-    for (let i = 0; i < gameboard.board.length; i++) {
-        if (gameboard.board[i][i] == 1) {
-            leftdiagsum++;
-        }
-        else if (gameboard.board[i][i] == -1) {
-            leftdiagsum--;
-        }
-
-        if (leftdiagsum == 3) {
-            return { xWins: true, yWins: false }
-        }
-        else if (leftdiagsum == -3) {
-            return { xWins: false, yWinds: true }
-        }
+    function CheckWinner(gameboard) {
+        
     }
 
-    for (let i = 0; i < gameboard.board.length; i++) {
-        let n = gameboard.board[i].length
-        if (gameboard.board[i][n - 1 - i] == 1) {
-            rightdiagsum++;
-        }
-        else if (gameboard.board[i][n - 1 - i] == -1) {
-            rightdiagsum--;
+    function Player(name) {
+        if (!new.target) {
+            return new Player(name);
         }
 
-        console.log(rightdiagsum)
-        if (rightdiagsum == 3) {
-            return { xWins: true, yWins: false }
-        }
-        else if (rightdiagsum == -3) {
-            return { xWins: false, yWinds: true }
-        }
+        this.name = name;
+        this.score = 0;
     }
 
 
-    return { xWins: false, yWins: false }
-}
-
-function Player(name) {
-    if (!new.target) {
-        return new Player(name);
-    }
-
-    this.name = name;
-    this.score = 0;
-}
-
-
-let gb = new Gameboard();
-let state = CheckBoardCondition(gb);
-console.log(state)
+    let gb = new Gameboard();
+    let state = CheckBoardCondition(gb);
+    console.log(state)
+} )();
