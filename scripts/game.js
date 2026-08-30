7c8d9a27f1c123a11157f818eb24cecfaaaf45bf
hszrdy <63145494+hszrdy@users.noreply.github.com>
2026-08-30 18:16:08 +0800

    logic overhaul
    changed loops to hardcoded condition checks, added eventlistener to grids for putting X or O and logic for adding in board

--- scripts/game.js
+++ scripts/game.js
@@ -2,116 +2,94 @@
     const xPlayerWinCondition = 3;
     const yPlayerWinCondition = -3;
     let turn = 1; //1 for x, -1 for y
-
-    function GenerateUIBoard() {
-        const gridContainer = document.querySelector('.grid-container');
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                const cell = document.createElement('div');
-                cell.addEventListener('click', (e) => {
-                    cell.textContent = turn === 1 ? 'X' : 'O';
-                    turn *= -1;
-                });
-                cell.classList.add('grid-item');
-                cell.dataset.row = i;
-                cell.dataset.col = j;
-                gridContainer.appendChild(cell);
-            }
-        }
-    }
+    let boardArray = new Array(9).fill(0);
 
     function Gameboard() {
         if (!new.target) {
             return new Gameboard();
         }
 
-        GenerateUIBoard();
-        this.board = [
-            [1, -1, -1],
-            [-1, -1, 1],
-            [-1, 1, 1]
-        ];
+        GenerateUIBoard(boardArray);
+        this.board = boardArray;
+        return this.board;
+    }
+
+
+    function GenerateUIBoard(arrayObj) {
+        const gridContainer = document.querySelector('.grid-container');
+        let n = 1;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const cell = document.createElement('div');
+
+                cell.classList.add('grid-item');
+                cell.dataset.index = n;
+                cell.dataset.clicked = false;
+
+                cell.addEventListener('click', (e) => {
+                    if(cell.dataset.clicked === 'true') {
+                        return;
+                    }
+                    cell.textContent = turn === 1 ? 'X' : 'O';
+                    turn *= -1;
+                    arrayObj[cell.dataset.index - 1] = turn;
+                    cell.dataset.clicked = true;
+                });
+                
+
+                gridContainer.appendChild(cell);
+                n++;
+            }
+        }
     }
 
     function CheckBoardCondition(gameboard) {
 
+        /* 
+            winning conditions:
+            1. horizontal
+                [1,2,3], [4,5,6], [7,8,9]
+            2. vertical
+                [1,4,7], [2,5,8], [3,6,9]
+            3. diagonal
+                [1,5,9], [3,5,7]
+        */
 
-        //check rows
-        for (let i = 0; i < gameboard.board.length; i++) {
-            let rowsum = 0;
-            for (let j = 0; j < gameboard.board[i].length; j++) {
-                if (gameboard.board[i][j] === 1) {
-                    rowsum++;
-                }
-                else if (gameboard.board[i][j] === -1) {
-                    rowsum--;
-                }
-            }
+        //check horizontals
+        let row1 = gameboard.board[0] + gameboard.board[1] + gameboard.board[2];
+        let row2 = gameboard.board[3] + gameboard.board[4] + gameboard.board[5];
+        let row3 = gameboard.board[6] + gameboard.board[7] + gameboard.board[8];
 
-            if (rowsum === 3) {
-                return { xWins: true, yWins: false }
-            }
-            else if (rowsum === -3) {
-                return { xWins: false, yWins: true }
-            }
+        if (row1 === xPlayerWinCondition || row2 === xPlayerWinCondition || row3 === xPlayerWinCondition) {
+            return 'X wins';
+        } else if (row1 === yPlayerWinCondition || row2 === yPlayerWinCondition || row3 === yPlayerWinCondition) {
+            return 'O wins';
         }
 
-        //check columns
-        for (let i = 0; i < gameboard.board.length; i++) {
-            let colsum = 0;
-            for (let j = 0; j < gameboard.board[i].length; j++) {
-                if (gameboard.board[j][i] === 1) {
-                    colsum++;
-                }
-                else if (gameboard.board[j][i] === -1) {
-                    colsum--;
-                }
-            }
 
-            if (colsum === 3) {
-                return { xWins: true, yWins: false }
-            }
-            else if (colsum === -3) {
-                return { xWins: false, yWins: true }
-            }
+        //check verticals
+        let col1 = gameboard.board[0] + gameboard.board[3] + gameboard.board[6];
+        let col2 = gameboard.board[1] + gameboard.board[4] + gameboard.board[7];
+        let col3 = gameboard.board[2] + gameboard.board[5] + gameboard.board[8];
+
+        if (col1 === xPlayerWinCondition || col2 === xPlayerWinCondition || col3 === xPlayerWinCondition) {
+            return 'X wins';
+        } else if (col1 === yPlayerWinCondition || col2 === yPlayerWinCondition || col3 === yPlayerWinCondition) {
+            return 'O wins';
         }
 
-        //check left root diagonals
-        let leftdiagsum = 0;
-        let rightdiagsum = 0;
-        for (let i = 0; i < gameboard.board.length; i++) {
-            if (gameboard.board[i][i] === 1) {
-                leftdiagsum++;
-            }
-            else if (gameboard.board[i][i] === -1) {
-                leftdiagsum--;
-            }
+        //check diagonals
+        let diag1 = gameboard.board[0] + gameboard.board[4] + gameboard.board[8];
+        let diag2 = gameboard.board[2] + gameboard.board[4] + gameboard.board[6];
 
-            if (leftdiagsum === 3) {
-                return { xWins: true, yWins: false }
-            }
-            else if (leftdiagsum === -3) {
-                return { xWins: false, yWins: true }
-            }
+        if (diag1 === xPlayerWinCondition || diag2 === xPlayerWinCondition) {
+            return 'X wins';
+        } else if (diag1 === yPlayerWinCondition || diag2 === yPlayerWinCondition) {
+            return 'O wins';
         }
 
-        for (let i = 0; i < gameboard.board.length; i++) {
-            let n = gameboard.board[i].length
-            if (gameboard.board[i][n - 1 - i] === 1) {
-                rightdiagsum++;
-            }
-            else if (gameboard.board[i][n - 1 - i] === -1) {
-                rightdiagsum--;
-            }
+        return 'No winner yet';
 
-            console.log(rightdiagsum)
-            if (rightdiagsum === 3) {
-                return { xWins: true, yWins: false }
-            }
-            else if (rightdiagsum === -3) {
-                return { xWins: false, yWins: true }
-            }
-        }
     }
 
     function CheckWinner(gameboard) {
