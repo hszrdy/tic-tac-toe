633eed06bc61f16db036372df485b4c2e125ccc8
hszrdy <63145494+hszrdy@users.noreply.github.com>
2026-09-06 02:08:05 +0800

    overhaul of game logic
    delegated tictactoecell generation to its own function
    
    added bunch of functions related to newround, restart, and startgame
    
    added functions related to show/hide board UI
    
    removed reduntant boolean change in CheckBoardConditions
    
    rearranged functions definition order

--- scripts/game.js
+++ scripts/game.js
@@ -3,10 +3,20 @@
     const yPlayerWinCondition = -3;
     let turn = 1; //1 for x, -1 for y
     let maxNumberOfTurns = 9;
-    let currentTurn = 1;
+    let currentTurn = 0;
     let isGameOver = false;
     let boardArray = new Array(9).fill(0);
+    let p1Score = 0;
+    let p2Score = 0;
+    let xPlayer = null;
+    let yPlayer = null;
 
+    function Player(name) {
+        if (!new.target) {
+            return new Player(name);
+        }
+        this.name = name;
+    }
 
     function CheckBoardCondition(gameboard) {
 
@@ -26,11 +36,9 @@
         let row3 = gameboard[6] + gameboard[7] + gameboard[8];
 
         if (row1 === xPlayerWinCondition || row2 === xPlayerWinCondition || row3 === xPlayerWinCondition) {
-            isGameOver = true;
             return 'X wins';
         } 
         else if (row1 === yPlayerWinCondition || row2 === yPlayerWinCondition || row3 === yPlayerWinCondition) {
-            isGameOver = true;
             return 'O wins';
         }
 
@@ -41,11 +49,9 @@
         let col3 = gameboard[2] + gameboard[5] + gameboard[8];
 
         if (col1 === xPlayerWinCondition || col2 === xPlayerWinCondition || col3 === xPlayerWinCondition) {
-            isGameOver = true;
             return 'X wins';
         } 
         else if (col1 === yPlayerWinCondition || col2 === yPlayerWinCondition || col3 === yPlayerWinCondition) {
-            isGameOver = true;
             return 'O wins';
         }
 
@@ -54,41 +60,72 @@
         let diag2 = gameboard[2] + gameboard[4] + gameboard[6];
 
         if (diag1 === xPlayerWinCondition || diag2 === xPlayerWinCondition) {
-            isGameOver = true;
             return 'X wins';
         } 
         else if (diag1 === yPlayerWinCondition || diag2 === yPlayerWinCondition) {
-            isGameOver = true;
             return 'O wins';
         }
 
         return 'No winner yet';
     }
 
-    function CheckWinner() {
-        if (currentTurn === maxNumberOfTurns) {
-            gameOver = true;
-            return alert('Game Over! The game is a draw!');
-        }
+    function UpdateScoreUI() {
+        const p1ScoreElement = document.getElementById('p1-wins');
+        const p2ScoreElement = document.getElementById('p2-wins');
 
+        p1ScoreElement.textContent = p1Score;
+        p2ScoreElement.textContent = p2Score;
+    }
+
+
+    function CheckWinner() {
 
         let state = CheckBoardCondition(boardArray);
 
         if (state === 'X wins') {
+            isGameOver = true;
+            p1Score++;
+            UpdateScoreUI();
             return alert('Game Over! X wins!');
         } else if (state === 'O wins') {
+            isGameOver = true;
+            p2Score++;
+            UpdateScoreUI();
             return alert('Game Over! O wins!');
         }
         
-    }
-
-    function Player(name) {
-        if (!new.target) {
-            return new Player(name);
+        if (currentTurn === maxNumberOfTurns) {
+            isGameOver = true;
+            return alert('Game Over! The game is a draw!');
         }
 
-        this.name = name;
-        this.score = 0;
+    }
+
+    function GenerateTicTacToeCell(arrayObj, index) {
+        const cell = document.createElement('div');
+
+        cell.classList.add('grid-item');
+        cell.dataset.index = index;
+        cell.dataset.clicked = false;
+
+        cell.addEventListener('click', (e) => {
+            if(cell.dataset.clicked === 'true') {
+                return;
+            }
+            if(isGameOver) {
+                return;
+            }
+            cell.textContent = turn === 1 ? 'X' : 'O';
+            cell.classList.add(turn === 1 ? 'red-mark' : 'blue-mark');
+            arrayObj[cell.dataset.index - 1] = turn;
+            turn *= -1;
+            currentTurn++;
+            cell.dataset.clicked = true;
+            CheckWinner();
+            console.log(arrayObj);
+        });
+            
+        return cell;
     }
 
 
@@ -97,28 +134,7 @@
         let n = 1;
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                const cell = document.createElement('div');
-
-                cell.classList.add('grid-item');
-                cell.dataset.index = n;
-                cell.dataset.clicked = false;
-
-                cell.addEventListener('click', (e) => {
-                    if(cell.dataset.clicked === 'true') {
-                        return;
-                    }
-                    if(isGameOver) {
-                        return;
-                    }
-                    cell.textContent = turn === 1 ? 'X' : 'O';
-                    arrayObj[cell.dataset.index - 1] = turn;
-                    turn *= -1;
-                    currentTurn++;
-                    cell.dataset.clicked = true;
-                    CheckWinner();
-                    console.log(arrayObj);
-                });
-                
+                const cell = GenerateTicTacToeCell(arrayObj, n);
 
                 gridContainer.appendChild(cell);
                 n++;
@@ -126,5 +142,79 @@
         }
     }
 
-    GenerateUIBoard(boardArray);
+    function HideBoard() {
+        const board = document.querySelector('.grid-container');
+        board.style.display = 'none';
+    }
+    
+    function ShowBoard() {
+        const board = document.querySelector('.grid-container');
+        board.style.display = 'grid';
+    }
+
+    function GameStart()
+    {
+        const p1Name = document.getElementById('player1-name-input').value;
+        const p2Name = document.getElementById('player2-name-input').value;
+        const name1 = document.getElementById('p1-name');
+        const name2 = document.getElementById('p2-name');
+
+        if (p1Name.trim() !== '')
+            name1.textContent = p1Name;
+        if (p2Name.trim() !== '')
+            name2.textContent = p2Name;
+
+        ShowBoard();
+    }
+
+    function NewRound()
+    {
+        boardArray.fill(0);
+        const grid = document.querySelector('.grid-container');
+        grid.replaceChildren();
+        boardArray.fill(0);
+        currentTurn = 0;
+        isGameOver = false;
+        turn = 1;
+        GenerateUIBoard(boardArray);
+        UpdateScoreUI();
+    }
+
+    function ResetGame(player1, player2)
+    {
+        NewRound();
+        p1Score = 0;
+        p2Score = 0;
+        UpdateScoreUI();
+    }
+
+    function Init()
+    {
+        xPlayer = new Player('Player 1');
+        yPlayer = new Player('Player 2');
+
+        GenerateUIBoard(boardArray);
+        HideBoard();
+
+        document.getElementById('p1-name').textContent = xPlayer.name;
+        document.getElementById('p2-name').textContent = yPlayer.name;
+
+        const startButton = document.getElementById('start-button');
+        startButton.addEventListener('click', () => { 
+            GameStart(xPlayer, yPlayer);
+        });
+
+        const newRoundButton = document.getElementById('new-round-button');
+        newRoundButton.addEventListener('click', () => {
+            NewRound(xPlayer, yPlayer);
+        });
+
+        const resetButton = document.getElementById('reset-button');
+        resetButton.addEventListener('click', () => {
+            ResetGame(xPlayer, yPlayer);
+        });
+
+    }
+
+    Init();
 } )();
